1b849d96a4bae144a3796f7a8c64a0b8613a9f3b
JR04 <175596598+0JR4@users.noreply.github.com>
2026-09-05 20:26:28 +0200

    Update validate_regions.py

--- validate_regions.py
+++ validate_regions.py
@@ -75,13 +75,10 @@ def check_json():
                     "roepnummer": item.get("Roepnummer", "Onbekend")
                 })
 
-   if invalid_entries:
+    if invalid_entries:
         send_discord_alert(invalid_entries)
         print(f"Fouten gevonden: {len(invalid_entries)} ongeldige regio('s). Berichten verstuurd naar Discord.")
-        
-        # OUD: sys.exit(1)  <-- Dit veroorzaakte de rode 'Error' in GitHub Actions
-        # NIEUW:
-        sys.exit(0) # Zorgt ervoor dat GitHub Actions het proces als geslaagd (groen) ziet
+        sys.exit(0)
     else:
         print("Alle regio's zijn correct gecontroleerd!")
 
